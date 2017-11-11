75d39fc0f83f82e4e000ec533be28ca39bacd4c4
Iain Coleman <iaincoleman@me.com>
2017-11-11 13:24:54 +0000

    Added login

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,26 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Iain Coleman on 11/11/2017.
+//  Copyright © 2017 Iain Coleman. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+    
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Iain Coleman on 11/11/2017.
+//  Copyright © 2017 Iain Coleman. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+
+let TO_LOGIN = "toLogin"
