8e18911900e2d307fd7b7d208787ba93b373f7e5
Iain Coleman <iaincoleman@me.com>
2017-11-11 13:49:32 +0000

    Added create account VC

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -1,5 +1,5 @@
 //
-//  LoginVC.swift
+//  CreateAccountVC.swift
 //  Smack
 //
 //  Created by Iain Coleman on 11/11/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class CreateAccountVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,13 +17,9 @@ class LoginVC: UIViewController {
         
     }
 
+    @IBAction func closePressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
     
 
-    @IBAction func closePressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction func createAccountBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
-    }
 }
