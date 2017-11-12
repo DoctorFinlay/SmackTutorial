f60d709c8946ffc7a982c5d7feccd176d337cef0
Iain Coleman <iaincoleman@me.com>
2017-11-12 15:05:07 +0000

    Added auth service to register user

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,9 +8,21 @@
 
 import Foundation
 
+//When we send a web request, we need to know if it completed or not - the completion handler is a closure that handles this
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
 //Segues
-
-
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+//URL Constants
+
+let BASE_URL = "https://billybig.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
